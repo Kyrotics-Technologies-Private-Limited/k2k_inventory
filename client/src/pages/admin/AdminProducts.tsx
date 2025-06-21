269d9cb89cc23e5676ecd3bd044f682fdb79f65a
risha328 <rishamondal328@gmail.com>
2025-06-21 19:12:37 +0530

    modalchange

--- client/src/pages/admin/AdminProducts.tsx
+++ client/src/pages/admin/AdminProducts.tsx
@@ -377,7 +377,7 @@ const AdminProductPage: React.FC = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-gray-800/50 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
           <div className="fixed inset-0 z-10 overflow-y-auto">
